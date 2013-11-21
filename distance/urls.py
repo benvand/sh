824b542002030294a781c6a9f38bc83ae2daa2e0
benvand <benjamin_vandersteen@hotmail.com>
2013-11-21 17:19:45 +0000

    tweaks, comments

--- distance/urls.py
+++ distance/urls.py
@@ -5,11 +5,11 @@ import config
 urlpatterns = patterns('',)
 
 if config.webpage:
-    urlpatterns += patterns('', url( 'distance/$', views.Distance.as_view(), ))#name='distance' ))
+    urlpatterns += patterns('', url( 'distance/$', views.Distance.as_view(), ))
 
 if config.api:
     api_prefix = "distance/api/"
-    urlpatterns += patterns('', url( '%s$' % api_prefix, views.Api.as_view(), ))# name='distance' ))
-    urlpatterns += patterns('', url( '%sip$' % api_prefix, views.ApiIP.as_view(), ))# name='distance' ))
-    urlpatterns += patterns('', url( '%spc$' % api_prefix, views.ApiPostcode.as_view(), ))# name='distance' ))
-    urlpatterns += patterns('', url( '%sll$' % api_prefix, views.ApiLatLong.as_view(), ))# name='distance' ))
+    urlpatterns += patterns('', url( '%s$' % api_prefix, views.Api.as_view(), ))
+    urlpatterns += patterns('', url( '%sip$' % api_prefix, views.ApiIP.as_view(), ))
+    urlpatterns += patterns('', url( '%spc$' % api_prefix, views.ApiPostcode.as_view(), ))
+    urlpatterns += patterns('', url( '%sll$' % api_prefix, views.ApiLatLong.as_view(), ))
